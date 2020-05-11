150df0c21cf620992d5a14a5b9a7e88dedab8a8b
Saleh Omar <salehomar82@gmail.com>
2020-05-11 13:50:09 +0200

    Assembler AB3 Verbesserung

--- Abgabe_Assembler_AB3/ggt.asm
+++ Abgabe_Assembler_AB3/ggt.asm
@@ -4,13 +4,16 @@ section .data
 section .text
 global CMAIN
 CMAIN:
-    mov ebp, esp; for correct debugging
-    mov eax, 30
-    mov ecx, 14
-    mov ebx, ecx
-    mov [result], eax   ;eax in result sichern
-schleife:
-    mov edx, 0
+    mov ebp, esp; for correct debuggingmov ebp, esp;
+    mov eax, 14
+    mov ecx, 30
+    cmp ecx, eax;a und c register vertauschen, falls c > a
+    jb  weiter
+    mov [result], eax
+    mov eax, ecx
+    mov ecx, [result]
+weiter:mov [result], eax   ;eax in result sichern
+schleife:mov edx, 0
     cmp ecx, 0
     je  fertig  ;verhindert teilen durch 0
     div ecx     ;edx eax / ecx
@@ -19,6 +22,5 @@ schleife:
     mov eax, [result] ;eax wiederherstellen fuer nächste rechnung
     mov ecx, edx    ;teiler auf modulo ergebnis setzten
     jmp schleife
-fertig:
-    mov [result], ecx   ;ergebnis in ecx
+fertig:mov [result], ecx   ;ergebnis in ecx
     ret
